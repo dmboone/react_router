a79a399cf5705d57d790f97272074ec81a6b96fd
Destiny Boone <destaj.db@gmail.com>
2023-04-12 13:54:29 -0400

    defining routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const myRouter = createBrowserRouter([
+  {path: '/', element: <HomePage />}
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={myRouter}></RouterProvider>;
 }
 
 export default App;
